b5f0699db7390bca504f8900d0aa8e401f274919
kreativae <hello@kreativ.ae>
2026-02-20 10:30:53 -0300

    subindo

--- bancodedados/connection.js
+++ bancodedados/connection.js
@@ -7,7 +7,7 @@
 
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/nexuscrm';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://kreativae:<db_password>@projeto-crm.xltdck4.mongodb.net/';
 
 const options = {
   maxPoolSize: 10,
